423a84867fa6ab089230a2051ce7e807d8c788e3
Danyl0-Tk <danylo.tkach@gmail.com>
2022-08-29 22:11:50 +0300

    fixing done btn, clearing code

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,31 +1,35 @@
-let tasksList = [];
 const inputEl = document.getElementById("inputField");
 const addBtn = document.getElementById("submitBtn");
 const ulEl = document.getElementById("ulElement");
 
-
 addBtn.addEventListener("click", addTask);
-ulEl.addEventListener("click", doneTask);
 
-function addTask () {
-  const todoDiv = document.createElement('div')
-  todoDiv.classList.add('todo')
+function addTask() {
+  //in order not to create empty task, we have to check whether input field isn`t empty
+  if (inputEl.value !== "") {
+    //creating div for li element and 'done task' button
+    const todoDiv = document.createElement("div");
+    todoDiv.classList.add("todo");
 
-  const liEl = document.createElement("li");
-  liEl.innerText = inputEl.value;
-  todoDiv.appendChild(liEl)
+    const liEl = document.createElement("li");
+    liEl.innerText = inputEl.value;
+    todoDiv.appendChild(liEl);
 
-  const doneBtn = document.createElement("button")
-  doneBtn.innerText = 'done'
-  todoDiv.appendChild(doneBtn)
+    const doneBtn = document.createElement("button");
+    doneBtn.innerText = "done";
+    todoDiv.appendChild(doneBtn);
+    doneBtn.addEventListener("click", doneTask);
 
-  ulEl.appendChild(todoDiv)
+    ulEl.appendChild(todoDiv);
 
-  inputEl.value = ''
+    inputEl.value = "";
+  } else {
+    console.log("Enter smth!");
+  }
 }
 
-function doneTask (e) {
-  const item = e.target
-  const todo = item.parentElement
-  todo.remove()
+function doneTask(e) {
+  const item = e.target; //returns button element itself
+  const todo = item.parentElement; //returns whole div element
+  todo.remove();
 }
