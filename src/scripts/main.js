e13bc8354b74a5b2c58c05c6d4c55b6500b5fbc9
Danyl0-Tk <danylo.tkach@gmail.com>
2022-08-31 19:42:54 +0300

    Added change theme btn

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,9 +1,11 @@
 const inputEl = document.getElementById("inputField");
 const addBtn = document.getElementById("submitBtn");
 const ulEl = document.getElementById("ulElement");
+const themeBtn = document.getElementById("themeBtn");
 
 window.addEventListener("load", renderTodoListLocal);
 addBtn.addEventListener("click", addTask);
+themeBtn.addEventListener("click", changeTheme);
 
 function addTask() {
   inputEl.setAttribute("placeholder", "");
@@ -12,7 +14,7 @@ function addTask() {
     //creating div for li element and 'done task' button
     const todoDiv = document.createElement("div");
     todoDiv.classList.add("todo");
-
+    
     const liEl = document.createElement("li");
     liEl.innerText = inputEl.value;
     todoDiv.appendChild(liEl);
@@ -61,7 +63,7 @@ function renderTodoListLocal() {
   todoList.forEach((task) => {
     const todoDiv = document.createElement("div");
     todoDiv.classList.add("todo");
-
+    
     const liEl = document.createElement("li");
     liEl.innerText = task;
     todoDiv.appendChild(liEl);
@@ -85,4 +87,24 @@ function removeTaskLocal(task) {
   const taskIndex = task.children[0].innerText;
   todoList.splice(todoList.indexOf(taskIndex), 1);
   localStorage.setItem("todoList", JSON.stringify(todoList));
+}
+
+function changeTheme() {
+  if (themeBtn.textContent === "L") {
+    themeBtn.textContent = "D";
+    document.body.classList.add("lightThemeBg");
+
+    const tasks = document.getElementsByClassName("todo");
+    for (let i = 0; i < tasks.length; i++) {
+      tasks[i].classList.add("lightThemeText");
+    }
+  } else {
+    themeBtn.textContent = "L";
+    document.body.classList.remove("lightThemeBg");
+
+    const tasks = document.getElementsByClassName("todo");
+    for (let i = 0; i < tasks.length; i++) {
+      tasks[i].classList.remove("lightThemeText");
+    }
+  }
 }
